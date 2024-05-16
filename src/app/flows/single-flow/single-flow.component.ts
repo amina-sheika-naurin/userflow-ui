511be8220f94fc5981d7977a48cbb1160286fca4
Amina Sheika Naurin <aminasheikanaurin@gmail.com>
2024-05-16 14:59:04 +0530

    highloght algo updated

--- src/app/flows/single-flow/single-flow.component.ts
+++ src/app/flows/single-flow/single-flow.component.ts
@@ -73,27 +73,53 @@ export class SingleFlowComponent {
   }
 
 
-  highlightArea(position: any) {
+  highlightArea(item: any) {
     const height = this.shape[0];
-    const width = this.shape[1];
+    let image:any = document.getElementById("image");
+    let aratio = parseInt(height)/parseInt(image?.offsetHeight)
+    this.locateNode(aratio,item)
+    // const width = this.shape[1];
 
-    this.aspecRatio = `${(height / width) * 100}%`;
-    console.log(this.aspecRatio)
+    // this.aspecRatio = `${(height / width) * 100}%`;
+    // console.log(this.aspecRatio)
 
-    const { row_min, row_max, column_min, column_max } = position;
+    // const { row_min, row_max, column_min, column_max } = position;
 
-    this.boxLeft = (column_min / width) * 100;
-    this.boxTop = (row_min / height) * 100;
-    this.boxWidth = ((column_max - column_min) / width) * 100;
-    this.boxHeight = ((row_max - row_min) / height) * 100;
+    // this.boxLeft = (column_min / width) * 100;
+    // this.boxTop = (row_min / height) * 100;
+    // this.boxWidth = ((column_max - column_min) / width) * 100;
+    // this.boxHeight = ((row_max - row_min) / height) * 100;
 
   }
 
+  locateNode(divider: any,item:any) {
+    let x = 0, y = 0;
+ 
+    if (item.position.column_min != 0)
+      x = item.position.column_min / divider;
+ 
+    if (item.position.row_min != 0)
+      y = item.position.row_min / divider;
+
+    let w = item.width;
+    // if (this.selDevice.technologyType.toLowerCase() === 'android') {
+      let canvas:any = document.getElementById("image");
+      let spy:any = document.getElementById("box");
+      // spy.style.border = "black";
+      spy.style.border = "#FF0000";
+      spy.style.borderStyle = "solid";
+      spy.style.position = "absolute";
+      spy.style.height = item.height/ divider + (canvas.offsetHeight - canvas.parentElement.clientHeight)+ 7 + "px";
+      spy.style.width = w / divider + (canvas.offsetWidth - canvas.parentElement.clientWidth) + "px";
+      spy.style.top = (y - 6)+ "px";
+      spy.style.left = (x - 2) + "px";
+      spy.style.boxShadow = "inset #f7e5e4 0px 0px 60px -12px";
+     
+  }
+
   removeHighlight() {
-    this.boxLeft = 0
-    this.boxTop = 0
-    this.boxWidth = 0
-    this.boxHeight = 0
+    let box:any = document.getElementById("box");
+    box.removeAttribute('style')
   }
 
   
